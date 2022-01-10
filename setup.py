8669222841f6d96be9042c6d34cfc29fb873266d
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-10 11:04:47 +0200

    v1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="old-norse-dictionary",
-    version="0.1.0",
+    version="1.0.0",
     author="stscoundrel",
     description="Old Norse Dictionary for Python. From The Cleasby & Vigfusson Old Norse to English Dictionary.",
     long_description=long_description,
