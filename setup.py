dd265047bf66feb07ef489e1b1818e061a72c83d
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-10 09:13:56 +0200

    Setup.py: populate project details

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="projectname",
-    version="1.0.0",
+    name="old-norse-dictionary",
+    version="0.1.0",
     author="stscoundrel",
-    description="Project desc",
+    description="Old Norse Dictionary for Python. From The Cleasby & Vigfusson Old Norse to English Dictionary.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/stscoundrel/python-template",
+    url="https://github.com/stscoundrel/old-norse-dictionary-py",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.9",
+    package_data={"": ["**/*.json"]},
 )
